6c86ef1dfd83be5c269aa10f64825e609c7d176e
ebin-antoo <ebin@ebin-antoo.com>
2014-12-11 13:11:36 -0500

    Added Collision and Effects

---
+++ Guardians-of-the-Galaxy/Scripts/managers/Collision.js
@@ -0,0 +1,48 @@
+function collision(AX1,AY1,AX2,AY2, BX1,BY1,BX2,BY2)
+{
+   if ( AX1 < BX1 )
+   {
+      if ( AX2 < BX1 )
+         return false;
+      else
+      {
+         if (AY1 < BY1)
+         {
+            if (AY2 < BY1)
+               return false;
+            else
+               return true;
+         }
+         else
+         {
+            if ( AY1 > BY2 )
+               return false;
+            else
+               return true;
+         }
+      }
+   }
+   else
+   {
+      if (AX1 > BX2)
+         return false;
+      else
+      {
+         if ( AY1 < BY1 )
+         {
+           if (AY2 < BY1)
+              return false;
+           else
+              return true;
+         }
+         else
+         {
+            if (AY1 > BY2)
+               return false;
+           else
+              return true;
+         }
+      }
+   }
+   return false; // should never get here
+}
